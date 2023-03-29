66a6ac4b122d8056e47340928d840c7284e4dfc1
Llyfr <k.dreiseitl@seznam.cz>
2023-03-29 11:45:01 +0200

    panda added to the set

--- sets.py
+++ sets.py
@@ -2,7 +2,7 @@ sets = {}
 
 
 # "Sheep Plushie","Teddy Bear Plushie", "Kitten Plushie",
-sets["Plushies"] = ["Red Fox Plushie", "Stingray Plushie", "Camel Plushie", "Red Fox Plushie", "Nessie Plushie", "Wolverine Plushie", "Chamois Plushie", "Monkey Plushie", "Lion Plushie", "Jaguar Plushie"]
+sets["Plushies"] = ["Red Fox Plushie", "Stingray Plushie", "Camel Plushie", "Panda Plushie", "Nessie Plushie", "Wolverine Plushie", "Chamois Plushie", "Monkey Plushie", "Lion Plushie", "Jaguar Plushie"]
 sets["Flowers"]  = ["Dahlia", "Orchid", "African Violet", "Cherry Blossom", "Peony", "Ceibo Flower", "Edelweiss", "Crocus", "Heather", "Tribulus Omanense", "Banana Orchid", ]
 
 
