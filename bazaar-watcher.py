32428e9bdcba61b93733b4cbfde052d880145856
Llyfr <k.dreiseitl@seznam.cz>
2023-03-29 15:08:43 +0200

    no longer crashes when internet is disconnected

--- bazaar-watcher.py
+++ bazaar-watcher.py
@@ -2,7 +2,7 @@ import datetime
 import json
 import os
 import time
-
+import sys
 import requests
 import schedule
 from colorama import init, Fore
@@ -19,10 +19,13 @@ init(autoreset=True)
 
 def get_request(url):
     while True:
-        json_info = requests.get(url).json()
-        if not json_info.get("error"):
-            return json_info
-        time.sleep(5)
+        try:
+            json_info = requests.get(url).json()
+            if not json_info.get("error"):
+                return json_info
+        except requests.exceptions.RequestException as e:
+            print(e)
+            time.sleep(5)
 
 
 # Clears console.
@@ -30,7 +33,40 @@ def cls():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 
-with open("setting.json") as json_data:  # loads data from file.
+def print_item(item, watched):  # Prints item info.
+    print(
+        f"There is {Fore.RED}{item.get('quantity')} {Fore.BLUE}{watched.get('name')} {Fore.RESET}on bazaar for {Fore.GREEN}${item.get('cost'):,}")
+
+
+def print_item_market(itemmarket, watched):
+    print(
+        f"There is {Fore.RED}{itemmarket[0].get('quantity')} {Fore.BLUE}{watched.get('name')} {Fore.RESET}on item market for {Fore.GREEN}${itemmarket[0].get('cost'):,}")
+
+
+def scan_bazaar(watch):
+    cls()
+    print("__________Update {}__________".format(datetime.datetime.now().time().strftime("%H:%M:%S")))
+    for watched in watch:
+        data = get_request(
+            "https://api.torn.com/market/{}?selections=bazaar,itemmarket&key={}".format(watched["id"], API_key))
+        bazaar = data.get("bazaar")
+        itemmarket = data.get("itemmarket")
+        for item in bazaar[0:3]:
+            if item.get("cost") <= watched.get("alert_bellow") and item.get("quantity") >= watched.get(
+                    "if_amount_bigger"):
+                print_item(item, watched)
+        if itemmarket[0].get("cost") <= watched.get("alert_bellow") and watched.get("if_amount_bigger") == 0:
+            print_item_market(itemmarket, watched)
+
+    print("__________End {}_____________".format(datetime.datetime.now().time().strftime("%H:%M:%S")))
+
+
+# Loads data from file. If no file is given it will load setting.json
+file = "setting.json"
+if len(sys.argv) > 1:
+    file = sys.argv[1]
+
+with open(file) as json_data:  # loads data from file.
     data = json.load(json_data)
 
 API_key = data.get("API_key")  # Get user API_key.
@@ -53,27 +89,9 @@ for watched in watch:
             watched["id"] = item
             if watched["alert_bellow"] <= 1: watched["alert_bellow"] *= items.get(item).get("market_value")
 
-for watched in watch:
-    print(watched)
 
-
-#
-def scan_bazaar():
-    cls()
-    print("__________Update {}__________".format(datetime.datetime.now().time()))
-    for watched in watch:
-        data = get_request("https://api.torn.com/market/{}?selections=bazaar,itemmarket&key={}".format(watched["id"], API_key))
-        bazaar = data.get("bazaar")
-        itemmarket = data.get("itemmarket")
-        for item in bazaar[0:3]:
-            if item.get("cost") <= watched.get("alert_bellow") and item.get("quantity") >= watched.get("if_amount_bigger"):
-                print(f"There is {Fore.RED}{item.get('quantity')} {Fore.BLUE}{watched.get('name')} {Fore.RESET}on bazaar for {Fore.GREEN}${item.get('cost'):,}")
-        if itemmarket[0].get("cost") <= watched.get("alert_bellow") and watched.get("if_amount_bigger") == 0:
-            print(f"There is {Fore.RED}{itemmarket[0].get('quantity')} {Fore.BLUE}{watched.get('name')} {Fore.RESET}on item market for {Fore.GREEN}${itemmarket[0].get('cost'):,}")
-
-if __name__ == "__main__":
-    scan_bazaar()
-    schedule.every(31).seconds.do(scan_bazaar)
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
+scan_bazaar(watch)
+schedule.every(31).seconds.do(scan_bazaar, watch=watch)
+while True:
+    schedule.run_pending()
+    time.sleep(1)
